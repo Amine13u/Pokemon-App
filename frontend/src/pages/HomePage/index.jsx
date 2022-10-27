6f6bdfb0cd5079cbccfe99b99c87cf886bec095a
Amine13u <46264764+Amine13u@users.noreply.github.com>
2022-10-27 12:37:23 +0100

    Adding Pokemons List

--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./index.css";
 
 function HomePage() {
-  return <div className="home-page">HomePage</div>;
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    getPokemonList();
+  }, [pokemonList]);
+
+  const getPokemonList = async () => {
+    try {
+      const { data } = await axios.get("/api");
+      setPokemonList(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="home-page">
+      <div className="left"></div>
+      <div className=" right">
+        <ul>
+          {pokemonList &&
+            pokemonList.map((pokemon) => (
+              <li className="pokemon">{pokemon}</li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
