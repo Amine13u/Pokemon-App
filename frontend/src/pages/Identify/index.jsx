d5b8552bb87b69d05d4748c32439a9c1df4e3a07
Amine13u <46264764+Amine13u@users.noreply.github.com>
2022-10-27 18:33:59 +0100

    Adding Identify Page

--- frontend/src/pages/Identify/index.jsx
+++ frontend/src/pages/Identify/index.jsx
@@ -1,7 +1,50 @@
+import { useState } from "react";
+import axios from "axios";
+import PokemonCard from "../../components/PokemonCard";
 import "./index.css";
 
 function Identify() {
-  return <div className="identify">Identify</div>;
+  const [pokemonType, setPokemonType] = useState("");
+  const [pokemonList, setPokemonList] = useState([]);
+
+  const getPokemonList = async (description) => {
+    try {
+      setPokemonList([]);
+      const { data } = await axios.get(
+        `/api/${description}/${
+          pokemonType[0].toUpperCase() + pokemonType.slice(1)
+        }`
+      );
+      setPokemonList(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    setPokemonType(e.target.value);
+  };
+
+  return (
+    <div className="identify">
+      <div className="form">
+        <input
+          placeholder="Please fill with Pokemon type"
+          onChange={(e) => handleChange(e)}
+        />
+        <button onClick={() => getPokemonList("strong")}>Strenth</button>
+        <button onClick={() => getPokemonList("weak")}>Weakness</button>
+      </div>
+      <div className="list">
+        {pokemonList &&
+          pokemonList.map((pokemon) => (
+            <div key={pokemon._id} className="item">
+              <PokemonCard pokemon={pokemon} />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default Identify;
